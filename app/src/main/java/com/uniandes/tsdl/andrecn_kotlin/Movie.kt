27ebdbe296148f1056d3e0e28ba801cc4d2e73a6
caev03 <ca.escobar2434@uniandes.edu.co>
2022-06-18 15:51:11 -0500

    Update webpage

--- app/src/main/java/com/uniandes/tsdl/andrecn_kotlin/Movie.kt
+++
@@ -1,3 +0,0 @@
-package com.uniandes.tsdl.andrecn_kotlin
-
-data class Movie(var title: String, var image: String)
--- app/src/main/java/com/uniandes/tsdl/andrecn_kotlin/SettingsActivity.java
+++
@@ -1,6 +0,0 @@
-package com.uniandes.tsdl.andrecn_kotlin;
-
-import android.app.Activity;
-
-public class SettingsActivity extends Activity {
-}
